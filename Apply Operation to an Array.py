47eb2cc13fc842297f00f792109183134e653686
burademissie <156226117+burademissie@users.noreply.github.com>
2024-05-18 04:44:39 -0700

    Create Apply Operation to an Array.py

---
+++ Apply Operation to an Array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        ans=[]
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1]:
+                nums[i]*=2
+                nums[i+1]=0
+        
+        for i in nums:
+            if i != 0 :
+                ans.append(i)
+        for i in nums:
+            if i== 0:
+                ans.append(0)
+        return ans
+           
+        return ans
+
+        
